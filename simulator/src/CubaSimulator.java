c71b6e8715e63fe19515dadf85b49ddbc557af90
information.without.borders <information.without.borders@8191e215-e11c-0410-9550-d3e4e145b1c8>
2006-09-18 03:37:14 +0000

    Got JavaDoc working.
    git-svn-id: https://sneakernet.googlecode.com/svn/trunk@3 8191e215-e11c-0410-9550-d3e4e145b1c8

---
+++ simulator/src/CubaSimulator.java
@@ -0,0 +1,16 @@
+/* Copyright (c) 2006 Information Without Borders
+ * Licensed under the GNU General Public License. 
+ */
+
+package org.iwb.simulator;
+
+/**
+ * This application simulates a sneakernet to Cuba.
+ */
+public class CubaSimulator
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Cuba Simulator");
+    }
+}
