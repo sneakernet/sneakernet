4dc70ca1ce75d515685e644b8f79e16d99141d1a
information.without.borders <information.without.borders@8191e215-e11c-0410-9550-d3e4e145b1c8>
2006-09-18 14:32:08 +0000

    SVN keyword expansion again.
    git-svn-id: https://sneakernet.googlecode.com/svn/trunk@6 8191e215-e11c-0410-9550-d3e4e145b1c8

---
+++ simulator/src/Simulator.java
@@ -0,0 +1,19 @@
+/* Copyright (c) 2006 Information Without Borders
+ * Licensed under the GNU General Public License. 
+ */
+
+package org.iwb.simulator;
+
+/**
+ * The Simulator class encapsulates functionality that allows a
+ * derived class to act as a sneakernet simulator, according to the
+ * simple model specified by the <a
+ * href="http://informationwithoutborders.endymion.com/index.php?title=Simulator_Code">Simulator
+ * Code</a> project.
+ * @see <a href="http://informationwithoutborders.endymion.com/index.php?title=Simulator_Code">Simulator Code</a>
+ * @author <a href="http://informationwithoutborders.endymion.com">Information Without Borders</a> by $Author$
+ * @version $Revision: 0 $
+ */
+public class Simulator
+{
+}
