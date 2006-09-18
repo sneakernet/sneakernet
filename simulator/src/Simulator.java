f281ce415521bc6e3a34875df686373793b78a89
information.without.borders <information.without.borders@8191e215-e11c-0410-9550-d3e4e145b1c8>
2006-09-18 18:58:55 +0000

    Set up some standard headers for Javadoc for all of the Java code to come.
    git-svn-id: https://sneakernet.googlecode.com/svn/trunk@8 8191e215-e11c-0410-9550-d3e4e145b1c8

--- simulator/src/Simulator.java
+++ simulator/src/Simulator.java
@@ -1,17 +1,18 @@
-/* Copyright (c) 2006 Information Without Borders
- * Licensed under the GNU General Public License. 
- */
-
 package org.iwb.simulator;
 
 /**
- * The Simulator class encapsulates functionality that allows a
- * derived class to act as a sneakernet simulator, according to the
- * simple model specified by the <a
+ * Provides functionality that allows a derived class to act as a
+ * sneakernet simulator, according to the simple model specified by
+ * the <a
  * href="http://informationwithoutborders.endymion.com/index.php?title=Simulator_Code">Simulator
  * Code</a> project.
+ * <p>
+ * Copyright (c) 2006 Information Without Borders Licensed under the
+ * GNU General Public License.
+ *
  * @see <a href="http://informationwithoutborders.endymion.com/index.php?title=Simulator_Code">Simulator Code</a>
- * @author <a href="http://informationwithoutborders.endymion.com">Information Without Borders</a> by $Author$
+ *
+ * @author $Author$ at <a href="http://informationwithoutborders.endymion.com">Information Without Borders</a>
  * @version $Revision$
  */
 public class Simulator
