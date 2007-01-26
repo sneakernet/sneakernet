775d9481c0141e4640b965d3c89cf0cf80c9aaad
information.without.borders <information.without.borders@8191e215-e11c-0410-9550-d3e4e145b1c8>
2007-01-26 18:08:22 +0000

    Added the first two screens in the design.
    git-svn-id: https://sneakernet.googlecode.com/svn/trunk@17 8191e215-e11c-0410-9550-d3e4e145b1c8

--- user/src/Sneakernet.java
+++ user/src/Sneakernet.java
@@ -19,20 +19,78 @@ public class Sneakernet extends MIDlet implements CommandListener
 {
     private Display mDisplay;
     private Command exitCmd;
-    private Form mForm;
+	private Command okayCmd;
+
+	private Form mLoginScreen;
+	private StringItem stringItem;
+	private TextField txtField;
+		
+	private Form mSubscribeToEmailScreen;
+	private StringItem mSubscribeString;
+	private TextField mSubscribeServer;
+	private TextField mSubscribeUserName;
+	private TextField mSubscribePassword;
+
+	public Sneakernet()
+	{
+	}
 	
     public void startApp()
-    {
-		if ( mForm == null )
+    {	
+		if( mLoginScreen == null )
 		{
-			mForm = new Form( "IWB Sneakernet" );
+			mLoginScreen = new Form( "Information Without Borders" );
+
+			stringItem = new StringItem("Sneakernet Login", null);
+			mLoginScreen.append(stringItem);
+
+			txtField = new TextField(
+			"Your Email Address: ", "", 50, TextField.ANY);
+			mLoginScreen.append(txtField);
+
+			txtField = new TextField(
+			"Your Password: ", "", 50, TextField.PASSWORD);
+			mLoginScreen.append(txtField);
+
 			exitCmd = new Command( "Exit", Command.EXIT, 0 );
-			mForm.addCommand( exitCmd );
-			mForm.setCommandListener( this );
-			mDisplay = Display.getDisplay( this );
+			mLoginScreen.addCommand( exitCmd );
+			mLoginScreen.setCommandListener( this );
+
+			okayCmd = new Command( "Okay", Command.OK, 0 );
+			mLoginScreen.addCommand( okayCmd );
+			mLoginScreen.setCommandListener( this );
 		}
-		
-		mDisplay.setCurrent( mForm );
+
+		if( mSubscribeToEmailScreen == null )
+		{
+			mSubscribeToEmailScreen = new Form( "Information Without Borders" );
+
+			mSubscribeString = new StringItem("Subscribe To Email", null);
+			mSubscribeToEmailScreen.append(mSubscribeString);
+
+			mSubscribeServer = new TextField(
+			"Incoming Mail Server: ", "", 50, TextField.ANY);
+			mSubscribeToEmailScreen.append(mSubscribeServer);
+
+			mSubscribeUserName = new TextField(
+			"User Name: ", "", 50, TextField.ANY);
+			mSubscribeToEmailScreen.append(mSubscribeUserName);
+
+			mSubscribePassword = new TextField(
+			"Password: ", "", 50, TextField.PASSWORD);
+			mSubscribeToEmailScreen.append(mSubscribePassword);
+
+			exitCmd = new Command( "Exit", Command.EXIT, 0 );
+			mSubscribeToEmailScreen.addCommand( exitCmd );
+			mSubscribeToEmailScreen.setCommandListener( this );
+
+			okayCmd = new Command( "Okay", Command.OK, 0 );
+			mSubscribeToEmailScreen.addCommand( okayCmd );
+			mSubscribeToEmailScreen.setCommandListener( this );
+		}
+
+		mDisplay = Display.getDisplay( this );
+		mDisplay.setCurrent( mLoginScreen );
 	}
 	
     public void pauseApp()
@@ -44,12 +102,27 @@ public class Sneakernet extends MIDlet implements CommandListener
     {
     }
 	
-	public void commandAction( Command c, Displayable s )
+	public void commandAction( Command mCommand, Displayable mScreen )
     {   
-        if ( c == exitCmd )
+		int iType = mCommand.getCommandType();
+
+		if( iType == Command.EXIT )
         {
             destroyApp( true );
             notifyDestroyed();
         }
+		else if( iType == Command.OK )
+		{
+			if( mScreen == mLoginScreen )
+			{
+				mDisplay = Display.getDisplay( this );
+				mDisplay.setCurrent( mSubscribeToEmailScreen );				
+			}
+			else
+			{
+				mDisplay = Display.getDisplay( this );
+				mDisplay.setCurrent( mLoginScreen );				
+			}			
+		}
 	}
 }
